3b3cc014be730645d881e14ad3ea48f3f6cb61dc
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-03-07 04:21:14 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const app = express()
+const port = 4000
+
+mongoose.connect('mongodb+srv://soniclinkerman:Sonic123@zaltick-pq9hg.mongodb.net/accountDB', {useNewUrlParser: true,  useUnifiedTopology: true });
+
+const accountSchema = new mongoose.Schema({
+    name: String,
+    username: String,
+    password: String
+})
+
+const Account = new mongoose.model("Account", accountSchema);
+
+app.use(express.static("public"));
+app.set("view engine", 'ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+
+app.post("/register", (req,res) => {
+    var newAccount = new Account({
+        name: req.body.name,
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    newAccount.save(function(err){
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.render("success")
+        }
+    })
+
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    Account.findOne({username: username}, function(err, foundUser) {
+        if(err) {
+            res.send(err)
+        }
+        else{
+            if(foundUser){
+                if(foundUser.password === password) {
+                    res.render("success")
+                }
+            }
+        }
+
+    })
+})
+
+
+app.listen(port, () => {
+    console.log(port)
+})
